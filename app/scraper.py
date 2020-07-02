7fb77a2705f1ec148d8babc1ca75e083546bec13
Jeff Minnear <jeffminnear@gmail.com>
2020-07-02 14:20:38 -0600

    Scrape GreenManGaming

--- app/scraper.py
+++ app/scraper.py
@@ -4,9 +4,15 @@ from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
 session = HTMLSession()
+session.browser
 
 
 def encode(s): return urllib.parse.quote(s)
+
+
+def get_soup(url):
+    r = session.get(url)
+    return BeautifulSoup(r.html.html, 'html.parser')
 
 
 class Scraper:
@@ -14,8 +20,7 @@ class Scraper:
     def steam(term):
         url = 'https://store.steampowered.com/search/?term=' + encode(term)
 
-        r = session.get(url)
-        soup = BeautifulSoup(r.html.html, 'html.parser')
+        soup = get_soup(url)
 
         results = []
 
@@ -32,6 +37,37 @@ class Scraper:
                       'title': title,
                       'price': (float(price) / 100),
                       'store': 1}
+            results.append(result)
+
+        return results
+
+    @staticmethod
+    def greenmangaming(term):
+        base_url = 'https://www.greenmangaming.com'
+        search_url = base_url + '/search/' + encode(term)
+
+        r = session.get(search_url)
+        r.html.render()
+        soup = BeautifulSoup(r.html.html, 'html.parser')
+
+        results = []
+
+        entries = soup.select('ul.table-search-listings > li')[:10]
+        for entry in entries:
+            media_object = entry.select('.media-object')[0]
+            link = media_object.select('a')[0]['ng-href']
+            img_url = media_object.select('img')[0]['ng-src']
+
+            title = entry.select('.prod-name')[0].text.strip()
+
+            price = entry.select('price.notranslate')[0].text.strip().replace('$', '')
+
+            result = {'link': base_url + link,
+                      'img_url': img_url,
+                      'title': title,
+                      'price': float(price),
+                      'store': 2}
+
             results.append(result)
 
         return results
